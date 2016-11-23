7a395276eaa290db2416500a17913bbbc30a651c
tkillman <dkkim1004@hanmail.net>
2016-11-23 21:28:40 +0900

    no message

---
+++ src/com/javalec/ex/FormEx.java
@@ -0,0 +1,70 @@
+package com.javalec.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
+/**
+ * Servlet implementation class FormEx
+ */
+@WebServlet("/FormEx")
+public class FormEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FormEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		request.setCharacterEncoding("EUC-KR");
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+		String[] hobbys=request.getParameterValues("hobby");
+		String major = request.getParameter("major");
+		
+		response.setContentType("text/html; charset=EUC-KR");
+		
+		PrintWriter writer= response.getWriter();
+		
+		writer.println("<html><head></head><body>");
+		writer.println("아이디 :"+id +"<br>");
+		writer.println("비밀번호 :"+pw +"<br>");
+		writer.println("취미 :"+Arrays.toString(hobbys) +"<br>");
+		writer.println("아이디 :"+id +"<br>");
+		writer.println("전공 :"+major +"<br>");
+		writer.println("</body></html>");
+		
+		
+		
+		
+		
+	}
+
+}
